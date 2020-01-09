0630f62ba0a89e1cc5e2f1741b7f536c1cf28d5a
Mlopezriveiro <56599157+Mlopezriveiro@users.noreply.github.com>
2020-01-09 13:10:55 +0100

    Add files via upload

---
+++ tarefa1/script.js
@@ -0,0 +1,16 @@
+/* 
+	Primeira tarefa
+	Nome do ficheiro: script.js
+	Usado: alert
+*/
+
+
+alert("Moi boas a todos");
+/* funciona */
+
+
+alert("2+2");
+/*  copia lo que sale en las comillas y lo pone en la alerta  */
+
+alert(2+2);
+/*   hace la suma     */
