1a06f9e666fc4f447694a9790573197e5c9a7cd2
Mlopezriveiro <56599157+Mlopezriveiro@users.noreply.github.com>
2020-01-14 17:00:50 +0100

    Add files via upload

---
+++ tarefa4/script.js
@@ -0,0 +1,14 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+var euSon = "Profe" + " que " + " conta " + " JavaScript ";
+alert(euSon);
+var oficio = "Profe";
+var longura = oficio.length;
+alert(longura);
+var rebanada = oficio.slice(3,5);
+alert(rebanada);
+var posicion = oficio.indexOf("f")
+alert(posicion);
