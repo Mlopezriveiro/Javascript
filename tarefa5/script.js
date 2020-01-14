82231886edf9eefe811f9e12027b03a2aecf9b60
Mlopezriveiro <56599157+Mlopezriveiro@users.noreply.github.com>
2020-01-14 18:13:18 +0100

    Add files via upload

---
+++ tarefa5/script.js
@@ -0,0 +1,25 @@
+/* 
+	Tarefa 5
+	Nome do ficheiro: script.js
+	Usado: prompt parseInt
+
+var nome;
+nome = prompt("Di algo   ");
+alert("Guau... que interesante " + nome);
+alert("En verdad no me importa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguen mais elegante ca " + nome);
+alert("e... " + nome + ", o mundo adorate!");
+*/
+
+var resposta;
+resposta = prompt("cantas ducias de ovos leva unha tortilla   ");
+
+var numero = parseInt(resposta);
+alert(numero);
+
+var ovos;
+
+ovos =12* numero;
+
+alert(ovos)
