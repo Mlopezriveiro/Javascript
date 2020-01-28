ed46c4364414db2d12f2a9e4f8f1e2bfbcc723b5
Mlopezriveiro <56599157+Mlopezriveiro@users.noreply.github.com>
2020-01-28 18:07:13 +0100

    Add files via upload

---
+++ caixa_veloz/script.js
@@ -0,0 +1,17 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+	*/
+	var primero
+	var segundo
+	var tercero
+	primero = prompt("inidca el precio");
+	primer = parseInt(primero);
+	segundo = prompt("inidca el precio");
+	segun = parseInt(segundo);
+	tercero = prompt("inidca el precio");
+	tercer = parseInt(tercero);
+	var total
+	total = primer + segun + tercer + "€" + "euros";
+	document.write(total)
